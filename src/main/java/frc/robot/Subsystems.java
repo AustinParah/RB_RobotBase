e27ced5e1e9211d2d4b5b2b86d14ae6cb8363656
AustinParah <parahaustin@gmail.com>
2025-01-03 15:38:47 -0600

    removed OMB_Rida subsystems

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -5,10 +5,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.async.AsyncManager;
 import frc.robot.auto.AutoManager;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Lifecycle;
-import frc.robot.subsystems.Shooter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,28 +28,13 @@ public class Subsystems {
     // Warning: This must be created after everything else to ensure all subsystems
     // are registered
     public static AutoManager autoManager;
-
     private static Subsystems instance;
-    public static Intake intakeSubsystem;
-    public static Shooter shooterSubsystem;
-    public static Feeder feederSubsystem;
 
     public Subsystems() {
         swerveSubsystem = TunerConstants.DriveTrain;
-        intakeSubsystem = new Intake();
-        shooterSubsystem = new Shooter();
-        feederSubsystem = new Feeder();
         createUtilitySubsystems();
 
         SmartDashboard.putData(CommandScheduler.getInstance());
-
-        lifecycleSubsystems.add(intakeSubsystem);
-        lifecycleSubsystems.add(feederSubsystem);
-        lifecycleSubsystems.add(shooterSubsystem);
-
-        SmartDashboard.putData("IntakeSubsystem", intakeSubsystem);
-        SmartDashboard.putData("ShooterSubsystem", shooterSubsystem);
-        SmartDashboard.putData("FeederSubsystem", feederSubsystem);
     }
 
     public static Subsystems getInstance() {
